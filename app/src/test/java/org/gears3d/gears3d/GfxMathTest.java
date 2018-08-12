19a4c45df0877f5d17b2aa796c4db83256b9fb61
Jordan Justen <jljusten@gmail.com>
2018-08-11 20:21:14 -0700

    math: Add test for frustum
    Signed-off-by: Jordan Justen <jljusten@gmail.com>

--- app/src/test/java/org/gears3d/gears3d/GfxMathTest.java
+++ app/src/test/java/org/gears3d/gears3d/GfxMathTest.java
@@ -1,5 +1,17 @@
+package org.gears3d.gears3d;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class GfxMathTest {
 
+    @Test
+    public void frustum() {
+        float[] f = GfxMath.frustum(-1.0, 1.0, 300, -300, 5.0, 200.0);
+        float[] expected =
+                { 5.0f, 0.0f, 0.0f, 0.0f, 0.0f, -0.01667f, 0.0f, 0.0f, 0.0f, -0.0f,
+                  -1.051282f, -1.0f, 0.0f, 0.0f, -10.256411f, 0.0f };
+        assertArrayEquals(expected, f, 0.01f);
+    }
 }
