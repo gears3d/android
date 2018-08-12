074b24b91271ddc2392e12c3164f30273fd03f1b
Jordan Justen <jljusten@gmail.com>
2018-08-11 20:14:30 -0700

    math: Add frustum function (with incorrect implementation)
    Signed-off-by: Jordan Justen <jljusten@gmail.com>

--- app/src/main/java/org/gears3d/gears3d/GfxMath.java
+++ app/src/main/java/org/gears3d/gears3d/GfxMath.java
@@ -1,4 +1,10 @@
 package org.gears3d.gears3d;
 
 public class GfxMath {
+    static float[] frustum(double left, double right, double bottom,
+                           double top, double nearVal, double farVal)
+    {
+        float[] f = new float[16];
+        return f;
+    }
 }
