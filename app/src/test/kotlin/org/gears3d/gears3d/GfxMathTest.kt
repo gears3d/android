52cf9cf874547282b495bf16751e2a46f6de32ce
Jordan Justen <jljusten@gmail.com>
2024-03-08 13:03:29 -0800

    gears3d: Convert GfxMathTest to Kotlin
    Signed-off-by: Jordan Justen <jljusten@gmail.com>

---
+++ app/src/test/kotlin/org/gears3d/gears3d/GfxMathTest.kt
@@ -0,0 +1,54 @@
+/* Jordan Justen : gears3d is public domain */
+
+package org.gears3d.gears3d
+
+import org.junit.Assert
+import org.junit.Test
+
+class GfxMathTest {
+    @Test
+    fun frustum() {
+        val f = GfxMath.frustum(-1.0, 1.0, 300.0, -300.0, 5.0, 200.0);
+        val expected = floatArrayOf(5.0f, 0.0f, 0.0f, 0.0f, 0.0f, -0.01667f,
+                                    0.0f, 0.0f, 0.0f, -0.0f, -1.051282f,
+                                    -1.0f, 0.0f, 0.0f, -10.256411f, 0.0f)
+        Assert.assertArrayEquals(expected, f.toFloatArray(), 0.01f)
+    }
+
+    @Test
+    fun mult_m4m4() {
+        val m1 = floatArrayOf(0.1f, -0.5f, -1.0f, 2.5f, -0.5f, 3.0f, -10f,
+                              7.0f, 4.2f, 6.3f, -7.1f, -8.8f, 1.8f, -2.2f,
+                              3.1f, -2.1f)
+        val m2 = floatArrayOf(5.3f, 0.3f, -1.4f, 5.6f, 3.3f, -2.4f, 7.1f,
+                              -2.9f, -6.4f, 2.3f, 8.6f, -4.4f, 6.6f, 3.7f,
+                              1.6f, -9.6f)
+        val m = GfxMath.mult_m4m4(m1.toTypedArray(), m2.toTypedArray())
+        val md = m.map { it.toDouble() }.toDoubleArray()
+        val expected =
+            doubleArrayOf(4.58, -22.889999, 18.999998, 15.910001,
+                         26.129997, 42.259998, -38.699997, -64.940002,
+                         26.409998, 73.960007, -91.300003, -66.340012,
+                         -11.750001, 39.0, -84.720001, 48.48)
+        Assert.assertArrayEquals(expected, md, 0.01)
+    }
+
+    @Test
+    fun translate() {
+        val m = GfxMath.translate(-1.0f, 2.0f, 3.0f)
+        val expected = floatArrayOf(1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+                                    0.0f, 0.0f, 0.0f, 1.0f, 0.0f, -1.0f, 2.0f,
+                                    3.0f, 1.0f)
+        Assert.assertArrayEquals(expected, m.toFloatArray(), 0.01f)
+    }
+
+    @Test
+    fun rotate() {
+        val m = GfxMath.rotate(1.0, -1.0, 2.0, 3.0)
+        val expected = floatArrayOf(1.0f, 1.605018f, -3.062035f, 0.0f,
+                                    -3.443808f, 2.379093f, 1.916715f, 0.0f,
+                                    0.303849f, 3.599657f, 4.677582f, 0.0f,
+                                    0.0f, 0.0f, 0.0f, 1.0f)
+        Assert.assertArrayEquals(expected, m.toFloatArray(), 0.01f)
+    }
+}
